1fb78c4413a05001e1c24b30d74f12fe583c522b
ssssota <tommysouta@gmail.com>
2021-08-19 00:20:28 +0900

    ♻️ Refactor fetching script

---
+++ scripts/utils.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// @ts-check
+import { getPalette } from 'colorthief';
+
+/**
+ * @param {import('googleapis').youtube_v3.Schema$ThumbnailDetails} thumbnailObject
+ * @returns {import('googleapis').youtube_v3.Schema$Thumbnail}
+ */
+export const getThumbnail = (thumbnailObject) =>
+	thumbnailObject.medium ?? // ~320x180
+	thumbnailObject.maxres ?? // ~1280x720
+	thumbnailObject.standard ?? // ~640x480
+	thumbnailObject.high ?? // ~480x360
+	thumbnailObject.default; // ~120x90
+
+/**
+ * @param {string} imgUrl
+ * @returns {Promise<string>}
+ */
+export const getDominantColor = async (imgUrl) => {
+	/** @type {[number, number, number][]} */
+	const [dominantColor] = await getPalette(imgUrl, 2, 2);
+	return `#${dominantColor.map((v) => v.toString(16).padStart(2, '0')).join('')}`;
+};
