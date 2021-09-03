e5d91161d49864ea50042e3b14ea3e9f7c6f88c8
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-03 19:53:01 +0900

    🐛 Firebase importing only browser mode

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,23 @@
 <script>
 	import { browser } from '$app/env';
-	import { initializeApp } from 'firebase/app';
-	import { getAnalytics, logEvent } from 'firebase/analytics';
 	import '../app.css';
 
 	if (browser) {
-		const firebaseConfig = {
-			apiKey: 'AIzaSyAOvJsv30R-sdRV1ekuU4uvJqOnUWJondI',
-			authDomain: 'hololive-music.firebaseapp.com',
-			projectId: 'hololive-music',
-			storageBucket: 'hololive-music.appspot.com',
-			messagingSenderId: '468454566898',
-			appId: '1:468454566898:web:678f4b5a3b875d8cdc996b',
-			measurementId: 'G-SYBPTKVGYL'
-		};
-
-		const app = initializeApp(firebaseConfig);
-		const analytics = getAnalytics(app);
-		logEvent(analytics, 'page_view');
+		Promise.all([import('firebase/app'), import('firebase/analytics')]).then(
+			([{ initializeApp }, { getAnalytics, logEvent }]) => {
+				const app = initializeApp({
+					apiKey: 'AIzaSyAOvJsv30R-sdRV1ekuU4uvJqOnUWJondI',
+					authDomain: 'hololive-music.firebaseapp.com',
+					projectId: 'hololive-music',
+					storageBucket: 'hololive-music.appspot.com',
+					messagingSenderId: '468454566898',
+					appId: '1:468454566898:web:678f4b5a3b875d8cdc996b',
+					measurementId: 'G-SYBPTKVGYL'
+				});
+				const analytics = getAnalytics(app);
+				logEvent(analytics, 'page_view');
+			}
+		);
 	}
 </script>
 
