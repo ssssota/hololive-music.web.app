f23a46350594b9c3114dbf9e33c4dbb2601d19f1
ssssota <tommysouta@gmail.com>
2021-12-11 17:48:22 +0900

    👷 add project toml option to env var

--- src/routes/data/project.ts
+++ src/routes/data/project.ts
@@ -2,7 +2,10 @@ import { readFileSync } from 'fs';
 import type { RequestHandler } from '@sveltejs/kit';
 import { parseProjectToml } from '$lib/data';
 
-const toml = readFileSync('project.toml', 'utf8');
+const toml = readFileSync(
+	import.meta.env.VITE_PROJECT_TOML_PATH?.toString() ?? 'project.toml',
+	'utf8'
+);
 export const parsedProject = parseProjectToml(toml);
 
 export const get: RequestHandler = () => {
