f31f1b4e9634ee24958fdcf02af6616e3c110d19
ssssota <tommysouta@gmail.com>
2021-12-11 17:27:27 +0900

    🚚 move fetch source

--- src/lib/data/parse.ts
+++ src/lib/data/parse.ts
@@ -1,33 +1,17 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-// @ts-check
 import TOML from '@iarna/toml';
+import type { Project, Playlist, ProjectInfo, Video } from './types';
 
-/**
- * @param {unknown} str Unknown object expect string
- * @param {string} path Property path
- * @returns {string}
- */
-const validateString = (str, path) => {
+const validateString = (str: unknown, path: string): string => {
 	if (typeof str === 'string') return str;
 	throw new Error(`Validation error ${path} is not string`);
 };
 
-/**
- * @param {unknown} str Unknown object expect nullable string
- * @param {string} path Property path
- * @returns {string | undefined}
- */
-const validateNullableString = (str, path) => {
+const validateNullableString = (str: unknown, path: string): string | undefined => {
 	if (str == null) return undefined;
 	return validateString(str, path);
 };
 
-/**
- * @param {unknown} arr Unknown object expect string array
- * @param {string} path Property path
- * @returns {string[]}
- */
-const validateStringArray = (arr, path) => {
+const validateStringArray = (arr: unknown, path: string): string[] => {
 	if (!Array.isArray(arr)) throw new Error(`Validation error ${path} is not array`);
 	const result = [];
 	for (const element of arr) {
@@ -36,13 +20,14 @@ const validateStringArray = (arr, path) => {
 	return result;
 };
 
-/**
- * @param {object} parsed parsed project.toml
- * @returns {import('./types').ProjectInfo}
- */
-const getProjectInfo = (parsed) => {
+const getProjectInfo = (parsed: TOML.JsonMap): ProjectInfo => {
 	const project = parsed['project'];
-	if (project == null || typeof project !== 'object')
+	if (
+		project == null ||
+		typeof project !== 'object' ||
+		Array.isArray(project) ||
+		project instanceof Date
+	)
 		throw new Error('Validation error project must be object');
 	return {
 		title: validateString(project['title'], 'project.title'),
@@ -54,17 +39,19 @@ const getProjectInfo = (parsed) => {
 	};
 };
 
-/**
- * @param {object} parsed parsed project.toml
- * @returns {import('./types').Playlist[]}
- */
-const getPlaylist = (parsed) => {
+const getPlaylist = (parsed: TOML.JsonMap): Playlist[] | undefined => {
 	if (parsed['playlist'] == null) return undefined;
 	const playlists = parsed['playlist'];
-	if (!Array.isArray(playlists))
-		throw new Error(`Validation error ${playlists} must be Playlist[]`);
+	if (!Array.isArray(playlists)) throw new Error(`Validation error playlists must be Playlist[]`);
 	const result = [];
 	for (const playlist of playlists) {
+		if (
+			playlist == null ||
+			typeof playlist !== 'object' ||
+			Array.isArray(playlist) ||
+			playlist instanceof Date
+		)
+			throw new Error('Validation error playlist must be object');
 		result.push({
 			id: validateString(playlist['id'], 'playlist.id'),
 			tags:
@@ -76,16 +63,14 @@ const getPlaylist = (parsed) => {
 	return result;
 };
 
-/**
- * @param {object} parsed parsed project.toml
- * @returns {import('./types').Video[]}
- */
-const getVideo = (parsed) => {
+const getVideo = (parsed: TOML.JsonMap): Video[] | undefined => {
 	if (parsed['video'] == null) return undefined;
 	const videos = parsed['video'];
 	if (!Array.isArray(videos)) throw new Error(`Validation error ${videos} must be Video[]`);
 	const result = [];
 	for (const video of videos) {
+		if (video == null || typeof video !== 'object' || Array.isArray(video) || video instanceof Date)
+			throw new Error('Validation error video must be object');
 		result.push({
 			id: validateString(video['id'], 'video.id'),
 			tags: video['tags'] == null ? undefined : validateStringArray(video['tags'], 'video.tags')
@@ -94,26 +79,25 @@ const getVideo = (parsed) => {
 	return result;
 };
 
-/**
- * @param {object} parsed parsed project.toml
- * @returns {{ id: string }[]}
- */
-const getIgnore = (parsed) => {
+const getIgnore = (parsed: TOML.JsonMap): { id: string }[] | undefined => {
 	if (parsed['ignore'] == null) return undefined;
 	const ignores = parsed['ignore'];
 	if (!Array.isArray(ignores)) throw new Error(`Validation error ${ignores} must be Ignore[]`);
 	const result = [];
 	for (const ignore of ignores) {
+		if (
+			ignore == null ||
+			typeof ignore !== 'object' ||
+			Array.isArray(ignore) ||
+			ignore instanceof Date
+		)
+			throw new Error('Validation error ignore must be object');
 		result.push({ id: validateString(ignore['id'], 'ignore.id') });
 	}
 	return result;
 };
 
-/**
- * @param {string} toml project.toml string
- * @returns {import('./types').Project}
- */
-export const parseProjectToml = (toml) => {
+export const parseProjectToml = (toml: string): Project => {
 	const parsed = TOML.parse(toml);
 
 	return {
