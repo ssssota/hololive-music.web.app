441b91bf6fae29bf2f268de9c4c534e81d648767
ssssota <tommysouta@gmail.com>
2022-03-24 23:29:04 +0900

    🐛 fix for prerendering with url searchParam

--- src/lib/store/tag.ts
+++ src/lib/store/tag.ts
@@ -1,8 +1,10 @@
 import { derived } from 'svelte/store';
 import { page } from '$app/stores';
+import { browser } from '$app/env';
 
 export const currentTag = derived(page, ({ url }) => {
 	try {
+		if (!browser) return [];
 		return url.searchParams.getAll('tag').filter((tag) => tag.trim() !== '');
 	} catch (e) {
 		console.warn(e);
